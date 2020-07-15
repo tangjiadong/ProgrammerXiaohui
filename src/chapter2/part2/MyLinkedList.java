5b8c8ab1a2fbe2fa963cf55c2dfad94c48ce7de0
Tang <tang897396324@163.com>
2020-07-15 22:43:09 +0800

    链表基本操作

--- src/chapter2/part2/MyLinkedList.java
+++ src/chapter2/part2/MyLinkedList.java
@@ -1,32 +1,124 @@
 package chapter2.part2;
 
-
-import org.w3c.dom.Node;
-
 /**
  * Created by Tang on 2020/7/15
  */
 public class MyLinkedList {
-    //头结点指针
+
+    //头节点指针
     private Node head;
     //尾节点指针
     private Node last;
-    //链表的实际长度
+    //链表实际长度
     private int size;
 
-    public void insert(int data, int index) throws Exception {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("超出链表节点范围!");
+    /**
+     * 链表插入元素
+     * @param index  插入位置
+     * @param data  插入元素
+     */
+    public void insert(int index, int data) throws Exception {
+        if (index<0 || index>size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围！");
         }
-        //Node insertedNode = new Node(data);
-        if (size == 0) {
+        Node insertedNode = new Node(data);
+        if(size == 0){
             //空链表
-        } else if (index == 0) {
+            head = insertedNode;
+            last = insertedNode;
+        } else if(index == 0){
             //插入头部
-        } else if (size == index) {
+            insertedNode.next = head;
+            head = insertedNode;
+        }else if(size == index){
             //插入尾部
-        } else {
+            last.next = insertedNode;
+            last = insertedNode;
+        }else {
             //插入中间
+            Node prevNode = get(index-1);
+            insertedNode.next = prevNode.next;
+            prevNode.next = insertedNode;
         }
+        size++;
+    }
+
+    /**
+     * 链表删除元素
+     * @param index  删除的位置
+     */
+    public Node remove(int index) throws Exception {
+        if (index<0 || index>=size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围！");
+        }
+        Node removedNode = null;
+        if(index == 0){
+            //删除头节点
+            removedNode = head;
+            head = head.next;
+        }else if(index == size-1){
+            //删除尾节点
+            Node prevNode = get(index-1);
+            removedNode = prevNode.next;
+            prevNode.next = null;
+            last = prevNode;
+        }else {
+            //删除中间节点
+            Node prevNode = get(index-1);
+            Node nextNode = prevNode.next.next;
+            removedNode = prevNode.next;
+            prevNode.next = nextNode;
+        }
+        size--;
+        return removedNode;
+    }
+
+    /**
+     * 链表查找元素
+     * @param index  查找的位置
+     */
+    public Node get(int index) throws Exception {
+        if (index<0 || index>=size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围！");
+        }
+        Node temp = head;
+        for(int i=0; i<index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    /**
+     * 输出链表
+     */
+    public void output(){
+        Node temp = head;
+        while (temp!=null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 链表节点
+     */
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.insert(0,3);
+        myLinkedList.insert(0,4);
+        myLinkedList.insert(2,9);
+        myLinkedList.insert(3,5);
+        myLinkedList.insert(1,6);
+        myLinkedList.remove(0);
+        myLinkedList.output();
     }
 }
